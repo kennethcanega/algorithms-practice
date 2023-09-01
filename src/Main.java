afe839bea899ec7ce80e4fd2c09efcb6e0cbcc13
Kenneth A. Canega <kacanega@UBVPNN5848.local>
2023-09-01 22:00:53 +0800

    Add selection sort

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] unsortedArray = {1, 8, 9, 44, 12, 15, 19, 23};
+        int[] unsortedArray = {1, 8, 9, 44, 12, 15, 19, 23, 29, 394, 92, 192, 99, 182, 94, 12, 42, 323, 42,
+                822, 134, 442, 382, 992, 984, 8482, 1000, 10428, 2872, 7393, 92323, 9292, 1, 3, 4, 88};
+
         int[] sortedArray = {1, 4, 8, 12, 15, 19, 24, 34, 43, 55, 70,
                 100, 104, 123, 142, 155, 183, 199, 200, 201, 202, 203, 204, 205, 206,
                 207, 299, 300, 344, 351, 400, 450, 490, 491, 493, 494, 500,
                 501, 502, 505, 599};
 
-        System.out.println("Linear Search: " + linearSearch(unsortedArray, 44));
-        System.out.println("Binary Search: " + binarySearch(sortedArray, 493));
+//        System.out.println("Linear Search: " + linearSearch(unsortedArray, 44));
+//        System.out.println("Binary Search: " + binarySearch(sortedArray, 493));
+        System.out.println("Selection Sort: " + Arrays.toString(selectionSort(unsortedArray)));
     }
 
 
@@ -52,5 +57,29 @@ public class Main {
             partitioned[i] = numbers[start++];
         }
         return partitioned;
+    }
+
+    private static int[] selectionSort(int[] numbers) {
+        int sortedIndex = 0;
+        int size = numbers.length;
+        int currentIndex = 0;
+        int minIndex = 0;
+        while (sortedIndex < size) {
+            if (numbers[currentIndex] < numbers[minIndex]) {
+                minIndex = currentIndex;
+            }
+
+            if (currentIndex == size - 1) {
+                int tempValue = numbers[sortedIndex];
+                numbers[sortedIndex] = numbers[minIndex];
+                numbers[minIndex] = tempValue;
+                sortedIndex++;
+                currentIndex = sortedIndex;
+                minIndex = sortedIndex;
+                continue;
+            }
+            currentIndex++;
+        }
+        return numbers;
     }
 }
